2f9fe46337c93d4ad103ee9e3ab0974e20fb629b
Common Lisper <atosdo@live.com>
2024-10-20 17:58:48 +0000

    add path validation, code separation

--- console_ui.py
+++ console_ui.py
@@ -1,17 +1,21 @@
+from collections.abc import Callable
+from pathlib import Path
+
 import validators
 
-from colors import Color
-from answer import Answer
 import extensions
+from answer import Answer
+from colors import Color
 
-C = Color()
+# for text coloring via console
+_C = Color()
 
 
 def show_welcome_message() -> None:
-    C.reset_style()
+    _C.reset_style()
 
     print(
-        C.text_green(
+        _C.text_green(
             """.:: Hello ans welcome to AudioBatchDownloader!
     You can download multiple files on the one web page with it!
     Also you can input many urls. ::.
@@ -20,12 +24,8 @@ def show_welcome_message() -> None:
     )
 
 
-def get_input(msg: str) -> str:
-    return input(msg).lower()
-
-
 def request_user_data() -> list[tuple[str, str, str]]:
-    print(C.text_magenta(".:: Please, input some data. ::.\n"))
+    print(_C.text_magenta(".:: Please, input some data. ::.\n"))
 
     inputs: list[tuple[str, str, str]] = []
     while True:
@@ -42,16 +42,34 @@ def request_user_data() -> list[tuple[str, str, str]]:
     return inputs
 
 
-def process_url() -> str:
+def process_input(input_msg: str, validator: Callable, error_msg: str) -> str:
     while True:
-        url = get_input(C.text_cyan("Enter the URL from which you wish to download files: "))
-        is_valid = validate_url(url)
+        inpt = get_input(_C.text_cyan(input_msg))
+        is_valid_inpt = validator(inpt)
 
-        if is_valid:
-            return url
+        if is_valid_inpt:
+            return inpt
 
-        print(C.text_red("The URL you entered is not valid, please enter it again"))
-        C.reset_style()
+        show_error(error=error_msg)
+
+
+def get_input(msg: str) -> str:
+    return input(msg).lower()
+
+
+def show_error(error: str) -> None:
+    print(_C.text_red(error))
+    _C.reset_style()
+
+
+def process_url() -> str:
+    url = process_input(
+        input_msg="Enter the URL from which you wish to download files: ",
+        validator=validate_url,
+        error_msg="The URL you entered is not valid, please enter it again",
+    )
+
+    return url
 
 
 def validate_url(url: str) -> bool:
@@ -64,15 +82,13 @@ def validate_url(url: str) -> bool:
 
 
 def process_extension() -> str:
-    while True:
-        extension = get_input(C.text_cyan("Select the type of file you wish to download (mp3, pdf, midi): "))
-        is_valid_extension = validate_extension(extension)
+    extension = process_input(
+        input_msg="Select the type of file you wish to download (mp3, pdf, midi): ",
+        validator=validate_extension,
+        error_msg="The file extension you entered is not valid, please enter it again",
+    )
 
-        if is_valid_extension:
-            return extension
-
-        print(C.text_red("The file extension you entered is not valid, please enter it again"))
-        C.reset_style()
+    return extension
 
 
 def validate_extension(ext: str) -> bool:
@@ -82,9 +98,28 @@ def validate_extension(ext: str) -> bool:
     return False
 
 
+def process_path() -> str:
+    path = process_input(
+        input_msg="Enter your computer's file storage path: ",
+        validator=validate_path,
+        error_msg="The path you entered is not valid, please enter it again",
+    )
+
+    return path
+
+
+def validate_path(path: str) -> bool:
+    p = Path(path)
+
+    if not p.is_dir():
+        return False
+
+    return True
+
+
 def show_process_message(url: str) -> None:
     print(f"\nProcessing => {url}")
 
 
 def show_finish_message() -> None:
-    print(C.text_magenta("Finished processing.\n"))
+    print(_C.text_magenta("Finished processing.\n"))
