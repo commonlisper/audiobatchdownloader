d9dd3f495c365ef4a8c67b1ee41f6d21badd4924
Common Lisper <atosdo@live.com>
2024-10-20 18:26:29 +0000

    add docs file extensions

--- extensions.py
+++ extensions.py
@@ -3,8 +3,9 @@
 AUDIO = ("mp3", "wav", "mid", "midi", "mpa", "ogg", "wma", "wpl")
 ARCHIVE = ("7z", "arj", "rar", "z", "zip")
 DATA = ("csv", "dat", "log", "db", "mdb", "sql", "xml")
+DOCS = ("pdf", "doc", "docx", "txt")
 EXE = ("bat", "bin", "com", "exe", "msi", "sh")
 IMAGE = ("bmp", "gif", "ico", "jpg", "jpeg", "png", "psd", "tif", "tiff", "webp")
 PRESENTATION = ("key", "odp", "pps", "ppt", "pptx")
 
-ALL = AUDIO + ARCHIVE + DATA + EXE + IMAGE + PRESENTATION
+ALL = AUDIO + ARCHIVE + DATA + DOCS + EXE + IMAGE + PRESENTATION
